b9872bc51bf23dbe2156f094844349a2f7b8dfed
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-02-05 15:06:47 +0100

    Added css files and wrote HTML partially

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Rafael Klaessen</title>
+    <meta charset="UTF-8">
+
+    <meta name="description" content="Hi, I'm Rafael Klaessen and this is my website! Click the link for more info about me.">
+    <meta name="keywords" content="Rafael Klaessen, Rafael, Klaessen, webdevelopment, webdesign, nederland webdevelopment, nederland webdesign, website bouwen, website laten maken">
+    <meta name="author" content="Rafael Klaessen">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow">
+    <!--Og data-->
+    <meta property="og:url" content="https://www.rafaelklaessen.nl">
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="Rafael Klaessen">
+    <meta property="og:description" content="Hi, I'm Rafael Klaessen and this is my website!">
+    <meta property="og:image" content="https://www.rafaelklaessen.nl/images/back.jpg">
+    <!--Twitter card-->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="https://www.rafaelklaessen.nl/">
+    <meta name="twitter:creator" content="@ra_ca02">
+    <meta name="twitter:title" content="rafaelklaessen.nl">
+    <meta name="twitter:description" content="Hi, I'm Rafael Klaessen and this is my website!">
+    <meta name="twitter:image" content="https://www.rafaelklaessen.nl/images/icons/favicon.png">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/main.css">
+
+  </head>
+  <body>
+    <header id="site-header" class="site-header">
+      <nav class="nav" role="navigation">
+        <ul class="nav-items">
+          <li class="nav-item">
+            <a href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Work</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header> 
+
+    <section id="site-intro" class="site-intro">
+      <h1 class="site-title section-title">Hi, I'm Rafael</h1>
+      <p class="site-subtext section-text">I'm a 14 y/o webdeveloper, based in the Netherlands.</p>
+    </section>
+
+    <section id="work" class="work">
+      <h1 class="section-title">
+        A collection of my work
+      </h1>
+      <section class="work-item-container">
+        <article class="work-item">
+          <figure class="work-preview" style="background-image: url('images/work/agoravm.png')"></figure>
+          <h4 class="work-title">AgoraVM</h4>
+        </article>
+        <article class="work-item">
+          <figure class="work-preview" style="background-image: url('images/work/mdc.png')"></figure>
+          <h4 class="work-title">Miljonairs Dating Club</h4>
+        </article>
+        <article class="work-item">
+          <figure class="work-preview" style="background-image: url('images/work/pin.png')"></figure>
+          <h4 class="work-title">Internet-Nation</h4>
+        </article>
+        <article class="work-item">
+          <figure class="work-preview" style="background-image: url('images/work/noodlesearching.png')"></figure>
+          <h4 class="work-title">NoodleSearching</h4>
+        </article>
+        <article class="work-item">
+          <figure class="work-preview" style="background-image: url('images/work/valeur.png')"></figure>
+          <h4 class="work-title">Valeur Psychologie</h4>
+        </article>
+        <article class="work-item">
+          <figure class="work-preview" style="background-image: url('images/work/roeland.png')"></figure>
+          <h4 class="work-title">Roeland Ruijsch Songwriting Studio</h4>
+        </article>
+      </section>
+    </section>
+
+    <section id="about" class="about">
+      <h1 class="section-title">
+        <div class="title-topline">But,</div>
+        I'm constantly learning and improving
+      </h1>
+      <p class="section-text">
+        Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text 
+      </p>
+    </section>
+
+    <section id="skills" class="skills">
+      <h1 class="section-title">
+        Technologies that I'm familiar with
+      </h1>
+      <section class="skill-item-container">
+        <article class="skill-item">
+          <img class="skill-image" src="images/skills/html5.png" alt="HTML5 logo">
+          <h3 class="skill-title">HTML5</h3>
+        </article>
+        <article class="skill-item">
+          <img class="skill-image" src="images/skills/sass.png" alt="Sass logo">
+          <h3 class="skill-title">Sass</h3>
+        </article>
+        <article class="skill-item">
+          <img class="skill-image" src="images/skills/javascript.png" alt="JavaScript logo">
+          <h3 class="skill-title">JavaScript (ES6)</h3>
+        </article>
+        <article class="skill-item">
+          <img class="skill-image" src="images/skills/phoenix.png" alt="Phoenix framework logo">
+          <h3 class="skill-title">Phoenix framework</h3>
+        </article>
+        <article class="skill-item">
+          <img class="skill-image" src="images/skills/php.png" alt="PHP logo">
+          <h3 class="skill-title">PHP</h3>
+        </article>
+      </section>
+    </section>
+
+  </body>
+</html>
